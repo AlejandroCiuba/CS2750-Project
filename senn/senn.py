05cb88a65b082200f67ff3d9dd0f7b707a16e016
Alejandro Ciuba <alejandrociuba@gmail.com>
2025-12-02 04:06:17 -0500

    Transferring the main SENN model

---
+++ senn/senn.py
@@ -0,0 +1,24 @@
+# SENN class
+from torch import nn
+
+import torch
+
+
+class SENN(nn.Module):
+
+    def __init__(self, concept_dim: int, emb_size: int,
+                 hidden: int = 64, pos_theta: bool = True):
+
+        super().__init__()
+        self.K = concept_dim
+        self.emb_size = emb_size
+        self.hidden_size = hidden
+        self.pos_theta = pos_theta
+
+        self.theta_net = nn.Sequential(
+            nn.Linear(self.emb_size, self.hidden_size),
+            nn.ReLU(),
+            nn.Linear(self.hidden_size, self.K),
+        )
+
+        self.bias = nn.Parameter(torch.zeros(1))
