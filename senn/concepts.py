2ec480b09ec88e0b74529fa56f668af4b2d81492
Alejandro <alejandrociuba@gmail.com>
2025-12-06 17:55:42 -0500

    Started sentence embedding features, but abandoned

--- senn/concepts.py
+++ senn/concepts.py
@@ -1,7 +1,20 @@
 # Fetch the concepst for the SENN
 from gensim.models import KeyedVectors
+from nltk.corpus import stopwords
 from nltk import word_tokenize
+from nltk import pos_tag
+from transformers import (
+    DistilBertModel,
+    DistilBertTokenizerFast,
+    RobertaModel,
+    RobertaTokenizerFast,
+    PreTrainedModel,
+    PreTrainedTokenizerFast,
+    PreTrainedTokenizer,
+)
 
+import concurrent.futures
+import string
 import torch
 
 """
@@ -9,6 +22,8 @@ Each function takes in a list of length N of sentences and outputs a Tensor
 of size NxK where each row is a feature vector 1xK. Additional args may be
 passed in through kwargs.
 """
+
+stop = stopwords.words('english')
 
 def cosine_similarity_between_neighbors(sents: list[str], **kwargs) -> torch.Tensor:
     """
@@ -36,6 +51,47 @@ def cosine_similarity_between_neighbors(sents: list[str], **kwargs) -> torch.Ten
     return torch.tensor(tops).squeeze()
 
 
+def cosine_similarity_sentence_top(sents: list[str], **kwargs) -> torch.Tensor:
+    """
+    embeddings: BERT model to get sentence embeddings
+    tokenizer: The BERT model's tokenizer
+    device: The device to have the BERT model on
+    The embeddings will be on the BERT model device.
+    """
+
+    def _masking(sent: str) -> tuple[str, list[str]]:
+        words_to_mask = [tok[0] for tok in pos_tag(word_tokenize(sent)) if tok[1] in ["JJ", "NN"]]
+        with concurrent.futures.ThreadPoolExecutor() as exec:
+            masked = exec.map(lambda tok: sent.replace(tok, ""), words_to_mask)
+        return sent, list(masked)
+    
+
+    def _get_cosines(sent: tuple[str, list[str]]):
+
+        def _get_embedding(input: str) -> torch.Tensor:
+            output = kwargs["tokenizer"](
+                input,
+                truncation=True,
+                padding="max_length",
+                max_length=128,
+                return_tensors="pt",
+                )
+            with torch.no_grad():
+                return kwargs["embeddings"](**output).last_hidden_state.mean(dim=-1)  # https://www.geeksforgeeks.org/nlp/how-to-generate-word-embedding-using-bert/
+
+        base_emb: torch.Tensor = _get_embedding(sent[0])
+        alt_embs: torch.Tensor = _get_embedding(sent[1])
+
+        return torch.nn.functional.cosine_similarity(base_emb, alt_embs, dim=-1).max().unsqueeze(dim=-1)
+
+
+    with concurrent.futures.ThreadPoolExecutor() as exec:
+        future = exec.map(lambda sent: _get_cosines(_masking(sent)), sents)
+
+
+    return torch.concat(tuple(future), dim=-1).unsqueeze(dim=-1)
+
+
 def no_concept(sents: list[str], **kwargs) -> torch.Tensor:
     return torch.ones(kwargs['k'])
 
@@ -47,8 +103,14 @@ if __name__ == "__main__":
         "I don't like that type of food.",
         "That's some awful disgusting smell...",]
     
-    vecs = KeyedVectors.load_word2vec_format("PATH", binary=True)
-    print(vecs.similarity("the", "this"))
+    # vecs = KeyedVectors.load_word2vec_format("PATH", binary=True)
+    # print(vecs.similarity("the", "this"))
 
-    top_k: torch.Tensor = cosine_similarity_between_neighbors(test, embeddings=vecs, top_k=3)
-    print(top_k)
+    # top_k: torch.Tensor = cosine_similarity_between_neighbors(test, embeddings=vecs, top_k=3)
+    # print(top_k)
+
+    embeddings = DistilBertModel.from_pretrained("distilbert-base-uncased", num_labels=2)
+    tokenizer = DistilBertTokenizerFast.from_pretrained("distilbert-base-uncased")
+
+    sent_sim: torch.Tensor = cosine_similarity_sentence_top(test, embeddings=embeddings, tokenizer=tokenizer, device="cpu")
+    print(sent_sim)
