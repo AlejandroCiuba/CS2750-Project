313e415e588ce0556a52cce47f5ec5fbfa5ff8ba
Alejandro Ciuba <alejandrociuba@gmail.com>
2025-12-07 02:54:03 -0500

    Fixed evaluation for eval_llms.py

--- llm-baselines/eval_llms.py
+++ llm-baselines/eval_llms.py
@@ -1,4 +1,7 @@
-# DESCRIPTION OF THE PROGRAM
+# Evaluate the LLMs.
+# NOTE: While we have the real answer in the original run_baselines.py output, we changed how we evaluated
+# the models (now matching any of the pleonasms, not just consensus, counts). Therefore, we need the data
+# and fold information.
 # Created by Alejandro Ciuba, alejandrociuba@pitt.edu
 from pathlib import Path
 
@@ -7,6 +10,7 @@ import json
 import logging
 import subprocess
 
+import pandas as pd
 
 def main(args: argparse.Namespace):
 
@@ -17,24 +21,28 @@ def main(args: argparse.Namespace):
 
     # Get total number of lines
     entries = int(subprocess.run(f"cat {args.output} | wc -l", shell=True, executable="/bin/bash", capture_output=True).stdout.decode(encoding='ascii'))  # https://www.baeldung.com/linux/python-run-bash-command
+    data = pd.read_json(args.data, lines=True)
+    data = data[data["fold"] == args.fold]
 
     # Check each line of JSON
+    count = 0
     correct = 0
     binary_correct = 0  # Whether or not it guess that there is a pleonasm
     with open(args.output, 'r') as src:
         for line in src:
             try:
                 obj = json.loads(s=line)
+                row: pd.Series = data.iloc[count, :]
+                if row['consensus'].strip().lower() != "neither":
+                    if str(obj['output']['pleonasm']).strip().lower() in [str(row['first']).strip().lower(), str(row['second']).strip().lower()]:
+                        correct += 1
+
+                if (str(obj['output']['pleonasm']).strip().lower() == "none" and "".join(obj['real']).strip().lower() == "none") \
+                or (str(obj['output']['pleonasm']).strip().lower() != "none" and "".join(obj['real']).strip().lower() != "none"):
+                    binary_correct += 1
+                count += 1
             except Exception as e:
                 logging.error(e)
-                continue
-
-            if str(obj['output']['pleonasm']).strip().lower() in [str(word).strip().lower() for word in obj['real']]:
-                correct += 1
-
-            if (str(obj['output']['pleonasm']).strip().lower() == "none" and "".join(obj['real']).strip().lower() == "none") \
-            or (str(obj['output']['pleonasm']).strip().lower() != "none" and "".join(obj['real']).strip().lower() != "none"):
-                binary_correct += 1
 
     logging.info(f"Accuracy: {correct/entries:.4f}\nBinary Accuracy: {binary_correct/entries:.4f}")
 
@@ -50,9 +58,25 @@ def add_args(parser: argparse.ArgumentParser):
     )
 
     parser.add_argument(
+        "-d",
+        "--data",
+        required=True,
+        type=Path,
+        help="File path to the original data (currentl assumes JSON).",
+    )
+
+    parser.add_argument(
+        "-f",
+        "--fold",
+        required=True,
+        type=int,
+        help="Fold to examine.",
+    )
+
+    parser.add_argument(
         "-s",
         "--save",
-        required=True,
+        required=False,
         type=Path,
         help="File path to save file.",
     )
